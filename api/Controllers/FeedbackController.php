07347dae4ee5572456fb50ad621060ec61195537
Akatsuki Gri <bizimgri19@gmail.com>
2025-04-29 17:25:07 +0300

    feedback and report features updated

--- api/Controllers/FeedbackController.php
+++ api/Controllers/FeedbackController.php
@@ -11,12 +11,33 @@ class FeedbackController extends MainController
 
     public function create()
     {
-        checkRequiredParams(["text"], $this->params);
-        $new_feedback = [
+        checkRequiredParams(["text", "url", "version", "type"], $this->params);
+        $inputs = [
             "personID" => AuthMiddleware::$person["id"],
-            "text" => $this->params["text"]
+            "text" => $this->params["text"],
+            "url" => $this->params["url"],
+            "version" => $this->params["version"],
+            "type" => $this->params["type"]
         ];
-        $result = $this->model->create($new_feedback, false);
+        if ($this->params["type"] == "r") {
+            $url = explode("/", $this->params["url"]);
+            if ($url[1] == "access") {
+                if ($url[2] == "data") {
+                    $inputs["entityType"] = "d";
+                    $data = (new VirtualModel("datas"))->getWhere(["vID" => $url[3]], "id");
+                    if (!empty($data[0])) {
+                        $inputs["entityId"] = $data[0]["id"];
+                    }
+                } else if ($url[2] == "profile") {
+                    $inputs["entityType"] = "p";
+                    $person = (new VirtualModel("persons"))->getWhere(["vID" => $url[3]], "id");
+                    if (!empty($person[0])) {
+                        $inputs["entityId"] = $person[0]["id"];
+                    }
+                }
+            }
+        }
+        $result = $this->model->create($inputs, false);
         if ($result) response(200);
         else response(500);
     }
