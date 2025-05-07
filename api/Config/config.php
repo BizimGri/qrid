0323afdd57ef92e7d384169d8fcbc7b34bd5dcd6
Akatsuki Gri <bizimgri19@gmail.com>
2025-05-07 18:24:54 +0300

    error log path, settings and htaccess update

--- api/Config/config.php
+++ api/Config/config.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '0');
+ini_set('log_errors', '1');
+ini_set('error_log', __DIR__ . '/../Storage/Logs/php_error.log');
 
 // .env dosyasını oku
 $envFile = dirname(__DIR__, 2) . '/api/.env';
